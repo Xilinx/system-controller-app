8dd30c6b3e158e5e6f3f752f5336baa1bb1a65a0
Saeed Nowshadi <saeed.nowshadi@xilinx.com>
2020-04-28 17:30:56 -0700

    The vccaux workaround should retry until it succeeds

--- src/vccaux_workaround_vck190.sh
+++ src/vccaux_workaround_vck190.sh
@@ -10,17 +10,14 @@ for pid in `pidof -x ${PNAME}`; do
 done
 
 while [ 1 ]; do
-	COUNT=5
-
 	# MIO37 == "DC_SYS_CTRL4"
 	MIO37=`gpiomon --format="%e" --num-events=1 gpiochip0 11`
 
 	# Falling edge (MIO37 == 0), Rising edge (MIO37 == 1)
 	/usr/bin/sc_app -c workaround -t vccaux -v ${MIO37} > /dev/null
 
-	# In case sc_app is busy, try few times before giving up
-	while [ $? -ne 0 -a ${COUNT} -ne 0 ]; do
+	# In case sc_app is busy, try until it succeeds
+	while [ $? -ne 0 ]; do
 		/usr/bin/sc_app -c workaround -t vccaux -v ${MIO37} > /dev/null
-		COUNT=`expr ${COUNT} - 1`
 	done
 done

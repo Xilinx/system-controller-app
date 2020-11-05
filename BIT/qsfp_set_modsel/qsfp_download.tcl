5ae8c8196b9c5df44ea808fad119eeadd77fee0f
Saeed Nowshadi <saeed.nowshadi@xilinx.com>
2020-11-04 19:30:03 -0800

    Add VMK180 PDI for enabling QSFP
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@xilinx.com>
    Acked-by: Sergei Korneichuk <sergei.korneichuk@xilinx.com>

--- BIT/qsfp_set_modsel/qsfp_download.tcl
+++ BIT/qsfp_set_modsel/qsfp_download.tcl
@@ -42,7 +42,7 @@ if {$idcode == "04CA8093"} {
 } elseif {$idcode == "04CAA093"} {
    device program vmk180_es1_system_wrapper.pdi
 } elseif {$idcode == "14CAA093"} {
-   puts "No PDI is available!"
+   device program vmk180_system_wrapper.pdi
 } else {
    puts "Invalid IDCODE!"
 }

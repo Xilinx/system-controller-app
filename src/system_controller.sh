13c475fc7b2d151c56e9028f6c419ab253822e7b
Saeed Nowshadi <saeed.nowshadi@xilinx.com>
2021-05-26 13:45:01 -0700

    Configure syslogd to log sc_app messages into a separate file
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@xilinx.com>
    Acked-by: Sergei Korneichuk <sergei.korneichuk@xilinx.com>

--- src/system_controller.sh
+++ src/system_controller.sh
@@ -1,3 +1,12 @@
 #! /bin/sh
 
+#
+# Configure syslogd to log sc_app messages into a separate file.
+#
+/bin/grep -e '^local3' /etc/syslog.conf
+if [ $? -ne 0 ]; then
+    /bin/echo 'local3.*  /var/log/local3.log' >> /etc/syslog.conf
+    /etc/init.d/syslog restart
+fi
+
 /usr/bin/sc_appd &

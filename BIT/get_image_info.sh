f437f768339fb3ed90af54aca741ea6209073222
Nowshadi, Saeed <saeed.nowshadi@amd.com>
2024-01-23 15:31:28 -1200

    Search for both 'CONFIG_MASTER' and 'pl_cfi' partitions in a PDI
    Some PDIs have 'unique_id' field in their 'CONFIG_MASTER' partition and some
    in their 'pl_cfi' partition.  Search for both partition names to find that
    unique id.
    
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@amd.com>
    
    Acked-by: Jonathan Stroud <jonathan.stroud@amd.com>

--- BIT/get_image_info.sh
+++ BIT/get_image_info.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (c) 2023 Advanced Micro Devices, Inc.  All rights reserved.
+# Copyright (c) 2023 - 2024 Advanced Micro Devices, Inc.  All rights reserved.
 #
 
 PDI=$1
@@ -15,6 +15,6 @@ if [ ! -f $PDI ]; then
    exit -1
 fi
 
-read -r image_id unique_id <<<$(bootgen -arch versal -read $PDI | grep -A1 "name (0x10) : CONFIG_MASTER" | tail -n 1 | awk '{print $4 " " $8}')
+read -r image_id unique_id <<<$(bootgen -arch versal -read $PDI | grep -A1 "name (0x10) : " | grep -A1 -E "pl_cfi|CONFIG_MASTER" | tail -n 1 | awk '{print $4 " " $8}')
 
 echo $image_id $unique_id

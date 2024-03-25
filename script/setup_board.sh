3e5de7e5efdc5e8d291b8819d18c1cb88b16e7cb
Jonathan Stroud <jonathan.stroud@amd.com>
2024-03-25 05:41:17 -1200

    Improve setup_board.sh script
    If we have a socks_proxy config file, then source it.
    Add error checking of dnf install command
    Automatically reboot after successful install of board package
    
    Signed-off-by: Jonathan Stroud <jonathan.stroud@amd.com>
    
    Acked-by: Saeed Nowshadi <saeed.nowshadi@amd.com>

--- script/setup_board.sh
+++ script/setup_board.sh
@@ -5,9 +5,16 @@
 #
 # SPDX-License-Identifier: MIT
 #
+if [ -f /etc/profile.d/socks_proxy.sh ]; then
+    . /etc/profile.d/socks_proxy.sh
+fi
 
 EEPROM=$(/bin/ls /sys/bus/i2c/devices/1-0054/eeprom 2> /dev/null || /bin/ls /sys/bus/i2c/devices/11-0054/eeprom 2> /dev/null)
 BOARD=$(/usr/sbin/ipmi-fru --fru-file="$EEPROM" --interpret-oem-data | /usr/bin/awk -F": " '/FRU Board Product/ { print tolower ($2) }')
 echo Install board pacakages for "$BOARD"
-dnf install -y "${BOARD}"
-echo "Install complete.  Reboot required"
+if dnf install -y "${BOARD}"; then
+    echo "Install complete. Automatically rebooting in 5s."
+    sleep 5 && reboot
+else
+    echo "Board install failed for ${BOARD}"
+fi

386841a9e586918c3b149f9d8ced191b01cab806
Saeed Nowshadi <saeed.nowshadi@amd.com>
2025-07-02 04:08:37 +0530

    Add more clock counters for VEK385
    Signed-off-by: Saeed Nowshadi <saeed.nowshadi@amd.com>
    Acked-by: Jesus De Haro <jesus.de-haro@amd.com>

--- script/versal_tcl_cmd.tcl
+++ script/versal_tcl_cmd.tcl
@@ -9,6 +9,10 @@
 source "/usr/share/system-controller-app/BIT/xsdb_funcs.tcl"
 
 versal_connect
+# XXX- need to revisit this workaround when full labtools support is available for T50.
+if {[string length [targets -nocase -filter {name =~ "*A78*"}]] != 0} {
+    rst -system
+}
 
 # Download the default PDI
 load_default_pdi [lindex $argv 0] [lindex $argv 1]
@@ -16,6 +20,11 @@ load_default_pdi [lindex $argv 0] [lindex $argv 1]
 # If the proc is 'load_default_pdi', we are done
 if {[lindex $argv 2] eq "load_default_pdi"} {
     exit
+}
+
+# T50 workaround in order for PL's address space could be accessed from xsdb
+if {[catch {targets -set -nocase -filter {name =~ "*A78*#0.0"}} err] == 0} {
+    rst -clear-registers -skip-activate-subsystem -processor
 }
 
 # Invoke the proc
